84e105a8f2ef5e39cf0d30bef3157ed886079d96
Terence Lee <hone02@gmail.com>
2015-06-25 16:58:01 -0400

    build ngx_mruby

--- build.sh
+++ build.sh
@@ -5,19 +5,45 @@ output_dir=$2
 cache_dir=$3
 
 nginx_tarball="nginx-$VERSION.tar.gz"
+ngx_mruby_dir="ngx_mruby"
+ngx_mruby_git="https://github.com/matsumoto-r/$ngx_mruby_dir.git"
 
 cd $cache_dir
 if [ ! -f $nginx_tarball ]; then
     echo "Downloading $nginx_tarball"
     curl -s -O -L "http://nginx.org/download/nginx-$VERSION.tar.gz"
 fi
+if [ ! -f $ngx_mruby_dir ]; then
+    echo "Cloning $ngx_mruby_git"
+    git clone $ngx_mruby_git
+    pushd $ngx_mruby_dir
+    git submodule init
+    git submodule update
+    popd
+else
+    pushd $ngx_mruby_dir
+    git submodule foreach git pull origin master
+    popd
+fi
 
 cd $workspace_dir
 tar zxf $cache_dir/$nginx_tarball
-cd nginx-$VERSION
-./configure --prefix=$workspace_dir/build --with-http_ssl_module
+cp -rf $cache_dir/$ngx_mruby_dir .
+
+nginx_src="$workspace_dir/nginx-$VERSION"
+ngx_mruby_src="$workspace_dir/$ngx_mruby_dir"
+
+pushd $ngx_mruby_dir
+./configure --with-ngx-src-root=$nginx_src
+make build_mruby
+make generate_gems_config
+popd
+
+cd $nginx_src
+./configure --prefix=$workspace_dir/build --with-http_ssl_module --add-module=$ngx_mruby_src --add-module=$ngx_mruby_src/dependence/ngx_devel_kit
 make
 make install
+
 cd $workspace_dir/build/sbin
 strip nginx
 mkdir -p $output_dir
