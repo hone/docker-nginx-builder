1c149cab1cba4d6d8654b9457dbe52f9e6ee0a69
Terence Lee <hone02@gmail.com>
2015-12-18 13:46:02 -0500

    nginx 1.9.2, 1.9.3, 1.9.7

---
+++ nginxes/cedar-14/nginx-1.9.2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source `dirname $0`/common.sh
+source `dirname $0`/../common.sh
+
+docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION=1.9.2 -e STACK=$STACK-e hone/nginx-builder:$STACK
