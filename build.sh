a272bf767bc4170ce46c4a457a988fa7d7aa5cc0
Terence Lee <hone02@gmail.com>
2016-09-15 11:07:25 -0500

    use our git subtree copy of ngx_mruby

--- build.sh
+++ build.sh
@@ -6,25 +6,13 @@ cache_dir=$3
 
 nginx_tarball="nginx-$VERSION.tar.gz"
 ngx_mruby_dir="ngx_mruby"
-ngx_mruby_git="https://github.com/matsumoto-r/$ngx_mruby_dir.git"
 
 cd $cache_dir
 if [ ! -f $nginx_tarball ]; then
     echo "Downloading $nginx_tarball"
     curl -s -O -L "http://nginx.org/download/nginx-$VERSION.tar.gz"
 fi
-if [ ! -d $ngx_mruby_dir ]; then
-    echo "Cloning $ngx_mruby_git"
-    git clone $ngx_mruby_git
-    pushd $ngx_mruby_dir
-    git submodule init
-    git submodule update
-    popd
-else
-    pushd $ngx_mruby_dir
-    git submodule foreach git pull origin master
-    popd
-fi
+
 cd $workspace_dir
 tar zxf $cache_dir/$nginx_tarball
 cp -rf $cache_dir/$ngx_mruby_dir .
@@ -34,9 +22,6 @@ ngx_mruby_src="$workspace_dir/$ngx_mruby_dir"
 
 pushd $ngx_mruby_dir
 ./configure --with-ngx-src-root=$nginx_src
-sed '28a\
-  conf.gem :core => \"mruby-eval\"\n' build_config.rb > new_build_config.rb
-mv new_build_config.rb build_config.rb
 make build_mruby
 make generate_gems_config
 popd
