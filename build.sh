6876ed8f4a7063e4d52558c1a9175d3f920166d9
Terence Lee <hone02@gmail.com>
2017-01-09 18:03:57 -0600

    build and package up ngx_pagespeed as a dynamic nginx module

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ cache_dir=$3
 nginx_tarball="nginx-$VERSION.tar.gz"
 ngx_mruby_dir="ngx_mruby"
 ngx_mruby_version=$(grep -oP '(?<=MODULE_VERSION ")(\d+\.\d+\.\d+)(?=")' $cache_dir/$ngx_mruby_dir/src/http/ngx_http_mruby_module.h | tr -d '\n')
+ngx_pagespeed_dir="ngx_pagespeed"
 
 cd $cache_dir
 if [ ! -f $nginx_tarball ]; then
@@ -27,8 +28,15 @@ make build_mruby
 make generate_gems_config
 popd
 
+mkdir -p $ngx_pagespeed_dir
+pushd $ngx_pagespeed_dir
+curl -O -f -L -sS https://ngxpagespeed.com/install
+chmod +x install
+./install -b $workspace_dir/$ngx_pagespeed_dir -m -v $NGX_PAGESPEED_VERSION
+popd
+
 cd $nginx_src
-./configure --prefix=$workspace_dir/build --with-http_ssl_module --with-debug --add-module=$ngx_mruby_src --add-module=$ngx_mruby_src/dependence/ngx_devel_kit
+./configure --prefix=$workspace_dir/build --with-http_ssl_module --with-debug --add-module=$ngx_mruby_src --add-module=$ngx_mruby_src/dependence/ngx_devel_kit --add-dynamic-module=$workspace_dir/$ngx_pagespeed_dir/ngx_pagespeed-$NGX_PAGESPEED_VERSION-beta
 make
 make install
 
@@ -36,3 +44,6 @@ cd $workspace_dir/build/sbin
 strip nginx
 mkdir -p $output_dir
 tar ckzf $output_dir/nginx-$VERSION-ngx_mruby-$ngx_mruby_version.tgz nginx
+
+cd $workspace_dir/build
+tar ckzf $output_dir/ngx_pagespeed-$NGX_PAGESPEED_VERSION.tgz modules/ngx_pagespeed.so
